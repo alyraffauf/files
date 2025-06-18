ce3116b7cba34ab11f40962320ba7f002d8f635f
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2025-06-18 16:22:26 +0700

    docs: some readme changes

--- dev/modules/readme.nix
+++ dev/modules/readme.nix
@@ -15,14 +15,16 @@
                 In-repository file generation flake-parts module
 
                 As Nix users, we naturally want
+                the source of all truth to reside within Nix files 📜
+                and other files that include that truth to be generated ⚡.
 
-                1. the source of all truth to reside within Nix files 📜
-
-                2. all other files to be generated ⚡
+                For example, who didn't want to
+                dynamically include various facts about their project
+                in the readme and make sure those are up-to-date?
 
                 but
 
-                1. project repositories are expected to include tracked readmes 📄
+                1. project repositories are expected to include _tracked_ readmes 📄
 
                 2. Git tracked or not, `.gitignore` files must sometimes exist 🤷
 
@@ -30,20 +32,18 @@
 
                 4. and the list goes on
 
-                You may be thinking 🤔
+                You may be thinking
 
-                > Wait, so what if they must be tracked—I can still
-                >
-                > 1. generate them from Nix
-                > 2. check that my repository is clean
+                > Wait 🤔, so what if they must be tracked—I can still
+                > generate them from Nix and then check that my repository is clean.
 
                 And you'd be right!
-                ..except that checking that your repository is clean cannot be a flake check.
+                ...except that checking that your repository is clean cannot be a flake check.
 
-                > Okay, so I make a flake check for each generated file
+                > Okay, so I'll make a flake check for each generated file.
 
                 Well, yes, you could.
-                ..or you could use this project—if you're using flake-parts, that is (sorry).
+                ...or you could use this project—if you're using flake-parts, that is (sorry).
               '';
         }
       ];
